785c3d7cc497fa94182f3cbc683dac10f48720d5
syedahalam <syedahalam@yahoo.com>
2021-10-27 13:08:02 -0400

    setup db models

--- backend/settings.py
+++ backend/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
+
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -140,4 +141,5 @@ REST_FRAMEWORK = {
     ]
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
+# added
+CORS_ALLOW_ALL_ORIGINS = True  
