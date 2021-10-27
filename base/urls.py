8da9305b16e38c5d636281bcbb9b89a0355e5a87
syedahalam <syedahalam@yahoo.com>
2021-10-27 12:34:40 -0400

    models added

--- base/urls.py
+++ base/urls.py
@@ -3,6 +3,19 @@ from . import views
 
 urlpatterns = [
     path('products/', views.ProductList.as_view(), name='product_list'),
-    path('products/<int:pk>',views.ProductDetail.as_view(), name='product_detail')
+    path('products/<int:pk>',views.ProductDetail.as_view(), name='product_detail'),
+
+    path('reviews/', views.ReviewList.as_view(), name='review_list'),
+    path('reviews/<int:pk>', views.ReviewDetail.as_view(), name='review_detail'),
+
+    path('orders/', views.OrderList.as_view(), name='order_list'),
+    path('orders/<int:pk>', views.OrderDetail.as_view(), name='order_detail'),
+
+    path('orderItems/', views.OrderItemList.as_view(), name='orderItem_list'),
+    path('orderItems/<int:pk>', views.OrderItemDetail.as_view(),
+    name='orderItem_detail'),
+
+    path('shippingAddresses/', views.ShippingAddressList.as_view(), name='shippingAddress_list'),
+    path('shippingAddresses/<int:pk>',views.ShippingAddressDetail.as_view(), name='shippingAddress_detail')
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,13 +1,53 @@
-from .models import Product
-from .serializers import ProductSerializer
+from .models import *
+from .serializers import *
 from rest_framework import generics
 # from .products import products
 
-
+# Products
 class ProductList(generics.ListCreateAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
 
 class ProductDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# Reviews
+class ReviewList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ReviewDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# orders
+class OrderList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# orderItems
+class OrderItemList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class OrderItemDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+# shipping Address
+class ShippingAddressList(generics.ListCreateAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+
+class ShippingAddressDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
